6265e7d52f02bea21e3de5b4745108c167302ccf
石鍋 伸和 <nobukazu.ishinabe@neu-inc.co.jp>
2026-01-27 21:04:01 +0900

    Hide header and footer

--- app.py
+++ app.py
@@ -17,28 +17,51 @@ st.set_page_config(
 )
 
 hide_streamlit_style = """
-            <style>
-            /* ヘッダー（上のバー）を消す */
-            header {
-                visibility: hidden !important;
-                height: 0px !important;
-            }
-            
-            /* フッター（Created by / Hosted with Streamlit）を消す */
-            footer {
-                visibility: hidden !important;
-                display: none !important;
-                height: 0px !important;
-            }
-            
-            /* その他、細かいデコレーションを消す */
-            [data-testid="stDecoration"] {
-                display: none !important;
-            }
-            [data-testid="stStatusWidget"] {
-                display: none !important;
-            }
-            </style>
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+				        .appview-container .main .block-container{
+                            padding-top: 1rem;
+                            padding-right: 3rem;
+                            padding-left: 3rem;
+                            padding-bottom: 1rem;
+                        }  
+                        .reportview-container {
+                            padding-top: 0rem;
+                            padding-right: 3rem;
+                            padding-left: 3rem;
+                            padding-bottom: 0rem;
+                        }
+                        header[data-testid="stHeader"] {
+                            z-index: -1;
+                        }
+                        div[data-testid="stToolbar"] {
+                        z-index: 100;
+                        }
+                        div[data-testid="stDecoration"] {
+                        z-index: 100;
+                        }
+                </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
